a4dd244261313e59ef230f19df3d0e3c762e7ca5
Chiispyn <dem.monsalves@duocuc.cl>
2025-10-02 15:53:16 -0300

    tyé

---
+++ typescriptTips/src/ejemplos/variables.ts
@@ -0,0 +1,12 @@
+let name:string="Wacoldo"
+var lastname:string="Soto"
+//const
+let numero:number = 1
+
+console.log(name)
+console.log(lastname)
+
+
+console.log("Nombre completo " + name + " "+ lastname)
+console.log(`Nombre completo: ${name} ${lastname}`);
+console.log(numero+1);
---
+++ typescriptTips/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+import './ejemplos/variables.ts'
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<h1> hola con typescript<h1>
+`
+
+
