d118be85630dcda845ac5a01f44b425a3075af6e
Chiispyn <dem.monsalves@duocuc.cl>
2025-10-03 09:42:02 -0300

    destructuracion

---
+++ typescriptTips/src/ejemplos/destructuracion.ts
@@ -0,0 +1,37 @@
+const persona={
+    nombre: "Wacoldo",
+    edad:40,
+    id: "waco"
+}
+
+console.log(persona);
+//const clave=persona.id
+//const nombre=persona.nombre
+//desestructurar los atributos en variables independientes
+const {id,nombre,edad}=persona;
+console.log(id,nombre,edad);
+
+//crear una interfaz para utilizar los datos
+interface Alumno{
+    id:string,
+    nombre:string,
+    edad:number,
+    carrera?:string | undefined
+}
+
+const useContext=({id,nombre,edad,carrera="Suspendido"}:Alumno)=>{
+    return {
+        key:id,
+        datosPersona:{
+            nombre:nombre,
+            edad
+        },
+        carrera
+    }
+}
+
+const contexto=useContext(persona)
+console.log(contexto);
+
+const {key,carrera,datosPersona:{nombre:usuario}}=contexto;
+console.log(key,carrera,usuario);
--- typescriptTips/src/main.ts
+++ typescriptTips/src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 //import './ejemplos/variables.ts'
-import './ejemplos/valoresLiterales.ts'
-import './ejemplos/colecciones.ts'
+//import './ejemplos/valoresLiterales.ts'
+//import './ejemplos/colecciones.ts'
+import './ejemplos/destructuracion.ts'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
